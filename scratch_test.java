ef1ec5366cbb3defba89bf03c138b58c494c8272
FranWDev <hernandezcrosafrancisco@gmail.com>
2026-04-20 21:58:36 +0100

    Fix: ordenacion alertas

--- scratch_test.java
+++
@@ -1,4 +0,0 @@
-package test;
-// Just checking if we can write a query like this in RecipeCookingAuditRepository:
-// @Query("SELECT rca FROM RecipeCookingAudit rca WHERE rca.correlationId IN (SELECT sl.correlationId FROM StockLedgerBatchDetail d JOIN d.ledgerTransaction sl WHERE d.batch.id = :batchId AND sl.correlationId IS NOT NULL)")
-// List<RecipeCookingAudit> findByBatchId(@Param("batchId") Long batchId);
